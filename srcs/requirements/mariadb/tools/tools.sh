1d73e78e6fe421cd51621441aea8ef9d2e98d574
adrgonza <adrian.gonser@icloud.com>
2024-01-09 01:53:07 +0100

    too many chnges

---
+++ srcs/requirements/mariadb/tools/tools.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo "CREATE DATABASE inceptionDatabase;" >> "mysql-init.txt"
+    echo "CREATE USER " >> "mysql-init.txt"
+    echo "CREATE USER " >> "mysql-init.txt"
+    echo "GRANT ALL PRIVILEGES ON " >> "mysql-init.txt"
+    echo "GRANT ALL PRIVILEGES ON " >> "mysql-init.txt"
+    echo "DROP USER " >> "mysql-init.txt"
+    echo "FLUSH PRIVILEGES;" >> "mysql-init.txt"
+    mysqld_safe --init-file=mysql-init.txt >/dev/null 2>&1
+else
+    mysqld_safe >/dev/null 2>&1
+fi
