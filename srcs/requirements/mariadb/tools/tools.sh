cee30d267bcb414f117d4a9f83204cb6591adee8
adrgonza <adrian.gonser@icloud.com>
2024-01-09 17:59:37 +0100

    docker containers now always up

--- srcs/requirements/mariadb/tools/tools.sh
+++ srcs/requirements/mariadb/tools/tools.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+mysql_install_db >/dev/null 2>&1
+
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
     echo "CREATE DATABASE inceptionDatabase;" >> "mysql-init.txt"
     echo "CREATE USER " >> "mysql-init.txt"
