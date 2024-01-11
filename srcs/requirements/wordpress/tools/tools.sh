6869c0cec1dfffee1701499bfa7b1cbd574f7f78
adrgonza <adrian.gonser@icloud.com>
2024-01-11 04:03:00 +0100

    reaching the end

--- srcs/requirements/wordpress/tools/tools.sh
+++ srcs/requirements/wordpress/tools/tools.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 
 sleep 2
 
@@ -7,8 +7,8 @@ chmod 777 wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --path=/wordpress
-wp config create --dbname="$MYSQL_DBNAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --path=/wordpress
-wp core install --url=$DOMAIN_NAME --title="AdrixelcrackSite" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com" --path=/var/www/html
-wp user create --path=/var/www/html "user" "mail" --role=author --user_pass="hola --allow-root
+wp config create --dbname="$MYSQL_DBNAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --path=/wordpress --force
+wp core install --url="$DOMAIN_NAME" --title="AdrixelcrackSite" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com" --path=/wordpress
+wp user create --path=/wordpress "user" "mail" --role=author --user_pass="hola" --allow-root
 
 php-fpm81 --nodaemonize
