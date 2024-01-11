c2c177f72ba12fcca6d94fd4f61490b59f95fabb
adrgonza <adrian.gonser@icloud.com>
2024-01-11 03:36:42 +0100

    mariadb properly installed

--- srcs/requirements/mariadb/tools/tools.sh
+++ srcs/requirements/mariadb/tools/tools.sh
@@ -1,16 +1,31 @@
-#! /bin/sh
+#!/bin/sh
 
+# Initialize the MySQL data directory
 mysql_install_db
 
-if [ ! -d "/var/lib/mysql/$MYSQL_DBNAME" ]; then
-    echo "CREATE DATABASE $MYSQL_DBNAME;" >> "mysql-init.txt"
-    echo "CREATE USER " >> "mysql-init.txt"
-    echo "CREATE USER " >> "mysql-init.txt"
-    echo "GRANT ALL PRIVILEGES ON $MYSQL_DBNAME" >> "mysql-init.txt"
-    echo "GRANT ALL PRIVILEGES ON $MYSQL_DBNAME" >> "mysql-init.txt"
-    echo "DROP USER " >> "mysql-init.txt"
-    echo "FLUSH PRIVILEGES;" >> "mysql-init.txt"
-    mysqld_safe --init-file=mysql-init.txt
-else
-    mysqld_safe
-fi
+# Start MariaDB server
+mysqld_safe &
+
+# Wait for MySQL to start (you can adjust the sleep duration based on your system)
+sleep 2
+
+# Run mysql_upgrade
+mysql_upgrade -u root -p"$MYSQL_ROOT_PASSWORD"
+
+# Create MySQL initialization script
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DBNAME;" >> "mysql-init.sql"
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "mysql-init.sql"
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DBNAME.* TO '$MYSQL_USER'@'%';" >> "mysql-init.sql"
+echo "FLUSH PRIVILEGES;" >> "mysql-init.sql"
+
+# Run MySQL initialization script
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" < mysql-init.sql
+
+# Remove sensitive information
+#rm -f mysql-init.sql
+
+# Stop MariaDB server
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+
+# Start MariaDB server with binding to all addresses
+mysqld --datadir="/var/lib/mysql" --socket="/var/run/mysqld/mysqld.sock" --user=mysql
--- srcs/requirements/wordpress/tools/tools.sh
+++ srcs/requirements/wordpress/tools/tools.sh
@@ -1,18 +1,14 @@
 #! /bin/sh
 
+sleep 2
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod 777 wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wpç
-
-while true; do
-    if mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -e "SELECT 1;" ; then
-        break
-    fi
-    sleep 1
-done
+mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --path=/wordpress
 wp config create --dbname="$MYSQL_DBNAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --path=/wordpress
-wp core install --url="$DOMAIN_NAME" --title="Your Site Title" --admin_user="admin" --admin_password="your_admin_password" --admin_email="admin@example.com" --path=/var/www/html
+wp core install --url=$DOMAIN_NAME --title="AdrixelcrackSite" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com" --path=/var/www/html
+wp user create --path=/var/www/html "user" "mail" --role=author --user_pass="hola --allow-root
 
 php-fpm81 --nodaemonize
