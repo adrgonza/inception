edea6dc5a5d4382e96fff37e9cebf1a960ce956a
adrgonza <adrian.gonser@icloud.com>
2024-01-12 04:27:45 +0100

    wordpress config always done

--- Makefile
+++ Makefile
@@ -1,24 +1,21 @@
 all: prune build
 
 build:
-	mkdir /home/adrgonza/data/wp || true
+	mkdir /home/adrgonza/data/wp /home/adrgonza/data/db || true
 	docker-compose -f ./srcs/docker-compose.yml up  --build
-#-d or --detach flag: Runs the containers in the background.
-#--build flag: Forces a rebuild of the Docker images specified in the docker-
 
 down:
 	docker compose -f ./scrs/docker-compose.yml down
 
 prune:
 	docker system prune -f
-#docker system prune -f: Removes unused Docker objects like containers, networks, images (both dangling and unreferenced), and optionally, volumes.
 
 stop:
 	docker stop $$(docker ps -a -q)
 
 clean:
 	docker rm -f $$(docker ps -a -q)
-	rm -rf /home/adrgonza/data/wp
+	rm -rf /home/adrgonza/data/wp /home/adrgonza/data/db
 
 cimages:	
 	docker rmi $$(docker images -a -q)
@@ -30,5 +27,3 @@ status:
 	docker ps -a
 
 .PHONY: build prune clean images status
-
-#$sudo systemctl start docker
--- srcs/requirements/wordpress/tools/tools.sh
+++ srcs/requirements/wordpress/tools/tools.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+if [ -f "/wordpress/wp-config.php" ]; then
+    echo "WordPress is already installed. Deleting existing configuration."
+    wp.phar config delete --path=/wordpress
+    rm -rf /wordpress/*
+fi
+
 sleep 10
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod 777 wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp.phar
 
-if [ ! -f "/wordpress/wp-config.php" ]; then
-	echo "Generating wp config"
-	wp.phar core download --path=/wordpress
-	wp.phar config create --dbname="$MYSQL_DBNAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --path=/wordpress --force
-	wp.phar core install --url="$DOMAIN_NAME" --title="AdrixelcrackSite" --admin_user="admin" --admin_password="admin" --admin_email="admin@gmail.com" --path=/wordpress
-	wp.phar user create --path=/wordpress "user" "adri@gmail.com" --role=author --user_pass="holaholaº" --allow-root
-fi
+wp.phar core download --path=/wordpress
+wp.phar config create --dbname="$MYSQL_DBNAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --path=/wordpress --force
+wp.phar core install --url="$DOMAIN_NAME" --title="AdrixelcrackSite" --admin_user="admin" --admin_password="admin" --admin_email="admin@gmail.com" --path=/wordpress
+wp.phar user create --path=/wordpress "user" "adri@gmail.com" --role=author --user_pass="holaholaº" --allow-root
 
-echo "Starting wordpress"
 php-fpm81 --nodaemonize
