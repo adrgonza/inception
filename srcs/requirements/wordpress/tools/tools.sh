0aa7854d3e2a141af5963bc560e65888a22293f7
adrgonza <adrian.gonser@icloud.com>
2024-01-16 04:44:44 +0100

    bug in wordpress fixed

--- srcs/requirements/wordpress/tools/tools.sh
+++ srcs/requirements/wordpress/tools/tools.sh
@@ -5,14 +5,14 @@ if [ -f "/wordpress/wp-config.php" ]; then #if a wordpress config exist delete i
     rm -rf /wordpress/*
 fi
 
+sleep 10 #waits for the data base to be created
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod 777 wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp.phar
 
-sleep 7 #waits for the data base to be created
-
 wp.phar core download --path=/wordpress #download esentials php files 
-wp.phar config create --dbname="$MYSQL_DBNAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --path=/wordpress #create a wp-config.php
+wp.phar config create --dbname="$MYSQL_DBNAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --path=/wordpress --force #create a wp-config.php
 wp.phar core install --url="$DOMAIN_NAME" --title="AdrixelcrackSite" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="admin@gmail.com" --path=/wordpress #this install wordpress
 wp.phar user create --path=/wordpress "$MYSQL_DBNAME" "adri@gmail.com" --role=author --user_pass="$WORDPRESS_DB_PASSWORD" --allow-root #this create a new user
 
